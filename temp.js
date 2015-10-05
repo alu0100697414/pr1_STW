07886954e0f0e32409bb21e8898e043ecba73430
alu0100697414 <alu0100697414@ull.edu.es>
2015-10-05 23:54:00 +0100

    Algunas pruebas mas con JS

--- temp.js
+++ temp.js
@@ -6,11 +6,11 @@ function conversor (){
   var ini_temp = document.getElementsByName("ini_temp")[0].value;
 
   // Expresion regular que vamos a usar.
-  var exp_regular = /\d+/;
+  var exp_regular = /(?:)(\d+)([cCfF]*)/;
 
   // Filtramos en la variable con la expresion regular.
   var value = ini_temp.match(exp_regular);
 
-  alert(value);
+  if(value != null) alert(value);
 
 };
