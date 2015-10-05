9fe6ad46c3a644061aad6b572e2c920188ab7bc7
alu0100697414 <alu0100697414@ull.edu.es>
2015-10-05 23:48:11 +0100

    Primeras pruebas con JS y las expresiones regulares

---
+++ temp.js
@@ -0,0 +1,16 @@
+"use strict"; // Sirve para dar mensajes de error y asi encontrarlos antes.
+
+function conversor (){
+
+  // Cogemos el valor del imput y lo guardamos.
+  var ini_temp = document.getElementsByName("ini_temp")[0].value;
+
+  // Expresion regular que vamos a usar.
+  var exp_regular = /\d+/;
+
+  // Filtramos en la variable con la expresion regular.
+  var value = ini_temp.match(exp_regular);
+
+  alert(value);
+
+};
