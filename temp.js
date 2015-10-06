5efb31ced028a77d28f83e1588bf45cdcd32daa4
alu0100697414 <alu0100697414@ull.edu.es>
2015-10-06 18:18:03 +0100

    Creada la expresion regular necesaria para la practica

--- temp.js
+++ temp.js
@@ -5,12 +5,18 @@ function conversor (){
   // Cogemos el valor del imput y lo guardamos.
   var ini_temp = document.getElementsByName("ini_temp")[0].value;
 
-  // Expresion regular que vamos a usar.
-  var exp_regular = /(?:)(\d+)([cCfF]*)/;
+  /* Expresion regular que acepta una cadena que empiece por un numero con el signo como manera opcional
+  asi como si este es decimal. Con el primer parentesis recojo el valor de la temperatura y con el segundo
+  el tipo de temperatura que es. */
+  var exp_regular = /(^[-+]?\d+(?:\.\d*)?)([fFcC])/;
 
   // Filtramos en la variable con la expresion regular.
   var value = ini_temp.match(exp_regular);
 
-  if(value != null) alert(value);
+  if(value != null){
+
+  } else {
+    alert("El valor '" + ini_temp + "' no es correcto. Lea las instrucciones.");
+  }
 
 };
